4ed53991cebf45de658169c7f0082f76fdf79fae
Ivan Emelyanov <vanoemel@gmail.com>
2024-11-13 12:33:34 +0300

    Добаил комментарий

--- Selenium_Auto_Stepik/selenium_2.4.7.py
+++ Selenium_Auto_Stepik/selenium_2.4.7.py
@@ -24,6 +24,7 @@ browser = webdriver.Chrome()
 browser.get("http://suninjuly.github.io/wait2.html")
 
 # говорим Selenium проверять в течение 5 секунд, пока кнопка не станет кликабельной
+# написал коммит
 button = WebDriverWait(browser, 5).until(EC.element_to_be_clickable((By.ID, "verify")))
 button.click()
 message = browser.find_element(By.ID, "verify_message")
