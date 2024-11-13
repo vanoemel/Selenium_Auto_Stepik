4f731f0549d3859468bfb6ca5a2bf7b97a57a5bd
IvanEmelyanov <69804939+vanoemel@users.noreply.github.com>
2024-11-13 12:40:32 +0300

    Update selenium_2.4.7.py
    убрал лишние строки

--- Selenium_Auto_Stepik/selenium_2.4.7.py
+++ Selenium_Auto_Stepik/selenium_2.4.7.py
@@ -1,18 +1,3 @@
-# from selenium import webdriver
-# from selenium.webdriver.common.by import By
-#
-# browser = webdriver.Chrome()
-# # говорим WebDriver ждать все элементы в течение 5 секунд
-# browser.implicitly_wait(5)
-#
-# browser.get("http://suninjuly.github.io/wait2.html")
-#
-# button = browser.find_element(By.ID, "verify")
-# button.click()
-# message = browser.find_element(By.ID, "verify_message")
-#
-# assert "successful" in message.text
-
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
