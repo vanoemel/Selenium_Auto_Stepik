597b799635d09d3be6a047109a0a0906a5276105
Ivan Emelyanov <vanoemel@gmail.com>
2024-11-13 12:05:25 +0300

    Решение работает. Добавлен комментарий к задаче по покупке дома за 100 баксов.

--- Selenium_Auto_Stepik/selenium_2.4.8.py
+++ Selenium_Auto_Stepik/selenium_2.4.8.py
@@ -14,7 +14,7 @@ browser = webdriver.Chrome()
 browser.get("http://suninjuly.github.io/explicit_wait2.html")
 
 try:
-
+    #Выжидаем 100
     WebDriverWait(browser, 12).until(EC.text_to_be_present_in_element((By.ID, "price"),"100"))
     button = browser.find_element(By.ID, "book")
     button.click()
